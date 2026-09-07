729a4c73f1354eec56093e2b4f3c46a566eef314
Rebecca Guimaraes <rebekagc03@gamil.com>
2026-09-07 22:34:03 +0200

    add JWT validation to gateway

---
+++ gateway-service/src/main/java/com/medilabo/gateway_service/security/JwtConfig.java
@@ -0,0 +1,30 @@
+package com.medilabo.gateway_service.security;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public JwtDecoder jwtDecoder(@Value("${jwt.secret}") String jwtSecret) {
+
+        SecretKey secretKey = new SecretKeySpec(
+                jwtSecret.getBytes(StandardCharsets.UTF_8),
+                "HmacSHA256"
+        );
+
+        return NimbusJwtDecoder.withSecretKey(secretKey)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+}
