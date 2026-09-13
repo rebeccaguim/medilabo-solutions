6db649ac3c2532a6a194f524b00237a6015afbe7
Rebecca Guimaraes <rebekagc03@gamil.com>
2026-09-13 22:06:40 +0200

    feat: initialize risk service

--- start-services.bat
+++ start-services.bat
@@ -6,6 +6,8 @@ start "Patient Service" cmd /k "cd /d %~dp0patient-service && mvn spring-boot:ru
 
 start "Notes Service" cmd /k "cd /d %~dp0notes-service && mvn spring-boot:run"
 
+start "Risk Service" cmd /k "cd /d %~dp0risk-service && mvn spring-boot:run"
+
 start "Gateway Service" cmd /k "cd /d %~dp0gateway-service && mvn spring-boot:run"
 
 start "Front Service" cmd /k "cd /d %~dp0front-service && mvn spring-boot:run"
